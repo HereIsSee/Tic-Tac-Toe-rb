0f631300c17e29f93bde230bfb8a224510958952
HereIsSee <biramantvydas@gmail.com>
2024-07-25 16:16:52 +0300

    Lint and fix code styling issues using RuboCop

--- lib/board.rb
+++ lib/board.rb
@@ -1,43 +1,64 @@
+# frozen_string_literal: true
+
+# The Board class represents a tic-tac-toe game board,
+# It includes methods for placing pieces,
+# checking for a win, stalemate and printing the board.
 class Board
-
-  # attr_accessor :board_array
-
   def initialize
-    @board_array = Array.new(9) { " " }
+    @board_array = Array.new(9) { ' ' }
   end
 
   def print_board
-    puts "-------------"
+    puts '-------------'
     puts "| #{@board_array[0]} | #{@board_array[1]} | #{@board_array[2]} |"
-    puts "-------------"
+    puts '-------------'
     puts "| #{@board_array[3]} | #{@board_array[4]} | #{@board_array[5]} |"
-    puts "-------------"
+    puts '-------------'
     puts "| #{@board_array[6]} | #{@board_array[7]} | #{@board_array[8]} |"
-    puts "-------------"
+    puts '-------------'
   end
 
-  def put position, marker
+  def put(position, marker)
     @board_array[position] = marker
     self
   end
-  def get position
+
+  def get(position)
     @board_array[position]
   end
 
-  def win?
-    return true if @board_array[0] == @board_array[1] && @board_array[1] == @board_array[2] && @board_array[0] != ' '
-    return true if @board_array[3] == @board_array[4] && @board_array[4] == @board_array[5] && @board_array[3] != ' '
-    return true if @board_array[6] == @board_array[7] && @board_array[7] == @board_array[8] && @board_array[6] != ' '
-    return true if @board_array[0] == @board_array[3] && @board_array[3] == @board_array[6] && @board_array[0] != ' '
-    return true if @board_array[1] == @board_array[4] && @board_array[4] == @board_array[7] && @board_array[1] != ' '
-    return true if @board_array[2] == @board_array[5] && @board_array[5] == @board_array[8] && @board_array[2] != ' '
-    return true if @board_array[0] == @board_array[4] && @board_array[4] == @board_array[8] && @board_array[0] != ' '
-    return true if @board_array[2] == @board_array[4] && @board_array[4] == @board_array[6] && @board_array[2] != ' '
+  def stalemate?
+    return true if @board_array.all? { |value| value != ' ' }
+
     false
   end
 
-  def stalemate?
-    return true if @board_array.all? { |value| value != " "}
+  def win?
+    check_condition1 || check_condition2 || check_condition3
+  end
+
+  private
+
+  def check_condition1
+    return true if @board_array[0] == @board_array[1] && @board_array[1] == @board_array[2] && @board_array[0] != ' '
+    return true if @board_array[3] == @board_array[4] && @board_array[4] == @board_array[5] && @board_array[3] != ' '
+    return true if @board_array[6] == @board_array[7] && @board_array[7] == @board_array[8] && @board_array[6] != ' '
+
+    false
+  end
+
+  def check_condition2
+    return true if @board_array[0] == @board_array[3] && @board_array[3] == @board_array[6] && @board_array[0] != ' '
+    return true if @board_array[1] == @board_array[4] && @board_array[4] == @board_array[7] && @board_array[1] != ' '
+    return true if @board_array[2] == @board_array[5] && @board_array[5] == @board_array[8] && @board_array[2] != ' '
+
+    false
+  end
+
+  def check_condition3
+    return true if @board_array[0] == @board_array[4] && @board_array[4] == @board_array[8] && @board_array[0] != ' '
+    return true if @board_array[2] == @board_array[4] && @board_array[4] == @board_array[6] && @board_array[2] != ' '
+
     false
   end
 end
--- main.rb
+++ main.rb
@@ -1,37 +1,38 @@
+# frozen_string_literal: true
+
 require_relative 'lib/board'
 
-def player_move player_number, player_sign, board
-  puts "Player #{player_number.to_s} make a move"
-  board.print_board
-
-  while true do
+def player_move(player_number, player_sign, board)
+  loop do
     position = gets.chomp.to_i
     if board.get(position) != ' '
-      puts "Invalid move! Player #{player_number.to_s} make a move again!"
+      puts "Invalid move! Player #{player_number} make a move again!"
     else
       board.put(position, player_sign)
       break
     end
   end
-  puts "Player #{player_number.to_s} wins!" if board.win?
-  board
 end
 
-
-puts "Do you want to play Tic-Tac-Toe?(yes/no3)"
-while gets.chomp == "yes" do
+puts 'Do you want to play Tic-Tac-Toe?(yes/no)'
+while gets.chomp == 'yes'
   board = Board.new
 
   number = 0
 
-  until board.win? || board.stalemate? do
+  until board.win? || board.stalemate?
     num = number % 2
-    num == 0 ? player_move(1, 'x', board) : player_move(2, 'o', board)
+    puts "Player #{num + 1} make a move"
+    board.print_board
+    num.zero? ? player_move(1, 'x', board) : player_move(2, 'o', board)
 
-    puts "Nobody wins :( !" if board.stalemate?
-    number+=1
+    if board.win?
+      puts "Player #{num} wins!"
+      board.print_board
+    end
+    puts 'Nobody wins :( !' if board.stalemate?
+    number += 1
   end
 
-
-  puts "Play again?"
+  puts 'Play again?'
 end
